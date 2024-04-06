4c3cbc8ecd379d3f2a055f661232da30606a25fc
Samit Sonkar <samitsonkar0@gmail.com>
2024-04-06 22:04:08 +0530

    adding sample data

--- init/index.js
+++ init/index.js
@@ -9,11 +9,11 @@ initDB();
 
 async function initDB() {
     await Listing.deleteMany({});
-    initData.data = initData.data.map((obj) => ({...obj, owner: "66068702d26a980b76483776" }));
+    initData.data = initData.data.map((obj) => ({...obj, owner: "661142fb79557ace92f12d70" }));
     await Listing.insertMany(initData.data)
     console.log("saved");
 }
 
 async function main() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+    await mongoose.connect("mongodb+srv://demouser:6sgF41253FDwoWHi@cluster0.erfrycw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 }
